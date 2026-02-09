16ca095449e41bf3b0f7164c182246ba137f4db2
vanyastaff <ivan.kondrashkin@gmail.com>
2026-02-08 18:50:19 -0600

    chore: remove flui-widgets and flui-objects crates

--- crates/flui-objects/src/media/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Media RenderObjects - images, video, etc.
-
-pub mod image;
-pub mod texture;
-
-pub use image::{ImageFit, RenderImage};
-pub use texture::{FilterQuality, RenderTexture, TextureId};
--- crates/flui-objects/src/viewport/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod abstract_viewport;
-pub mod shrink_wrapping_viewport;
-
-pub use abstract_viewport::{RenderAbstractViewport, RevealedOffset, DEFAULT_CACHE_EXTENT};
-pub use shrink_wrapping_viewport::RenderShrinkWrappingViewport;
